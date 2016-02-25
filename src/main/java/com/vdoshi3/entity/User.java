e856c686b43fff3950f776f09ffbef50ce0c811c
Vishal Doshi <visdos007@gmail.com>
2016-02-24 20:51:38 -0600

    refactor(User): Add annotations for primary key and column constraint

--- src/main/java/com/vdoshi3/entity/User.java
+++ src/main/java/com/vdoshi3/entity/User.java
@@ -6,15 +6,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @ToString
+@Entity
+@Table
 public class User {
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator( name = "uuid", strategy ="uuid2")
 	private int uid;
 	private String uname;
+	@Column(unique = true)
 	private String email;
+	@Column(unique = true)
 	private String userid;
 	private String upassword;
 	private String salt;
