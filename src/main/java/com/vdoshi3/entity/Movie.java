c7bf02b5cd175edd2205e13870fc085b4da30a64
Vishal Doshi <visdos007@gmail.com>
2016-02-27 17:01:04 -0600

    feat(Movie): Create a Movie Entity

--- src/main/java/com/vdoshi3/entity/Movie.java
+++ src/main/java/com/vdoshi3/entity/Movie.java
@@ -2,13 +2,25 @@ package com.vdoshi3.entity;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @ToString
+@Entity
+@Table
 public class Movie {
-	private int mid;
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String mid;
 	private String title;
 	private int myear;
 	private String rated;
