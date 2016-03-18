5722ae58d83b5d251ce0b9fd88ab6d8a1f091f7b
Vishal Doshi <visdos007@gmail.com>
2016-03-18 04:43:20 -0500

    feat(DeleteMovie): Implement deleting comments and rating before actual movie

--- src/main/java/com/vdoshi3/service/MovieServiceImp.java
+++ src/main/java/com/vdoshi3/service/MovieServiceImp.java
@@ -7,8 +7,12 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vdoshi3.dao.CommentDao;
 import com.vdoshi3.dao.MovieDao;
+import com.vdoshi3.dao.RatingDao;
+import com.vdoshi3.entity.Comment;
 import com.vdoshi3.entity.Movie;
+import com.vdoshi3.entity.Rating;
 import com.vdoshi3.exception.ResourceAlreadyExistsException;
 import com.vdoshi3.exception.ResourceNotFoundException;
 
@@ -17,12 +21,16 @@ import com.vdoshi3.exception.ResourceNotFoundException;
 public class MovieServiceImp implements MovieService {
 	@Autowired
 	MovieDao repo;
-	
+	@Autowired
+	CommentDao commentRepo;
+	@Autowired
+	RatingDao ratingRepo;
+
 	@Override
 	public Movie create(Movie movie) throws ResourceAlreadyExistsException {
-		if (repo.findById(movie.getMid()) == null){
+		if (repo.findById(movie.getMid()) == null) {
 			return repo.create(movie);
-		}else{
+		} else {
 			throw new ResourceAlreadyExistsException();
 		}
 	}
@@ -35,9 +43,9 @@ public class MovieServiceImp implements MovieService {
 	@Override
 	public Movie findById(String mid) throws ResourceNotFoundException {
 		Movie m = repo.findById(mid);
-		if (m != null){
+		if (m != null) {
 			return m;
-		}else{
+		} else {
 			throw new ResourceNotFoundException();
 		}
 	}
@@ -49,9 +57,9 @@ public class MovieServiceImp implements MovieService {
 
 	@Override
 	public Movie update(String mid, Movie movie) throws ResourceNotFoundException {
-		if (repo.findById(mid) != null){
+		if (repo.findById(mid) != null) {
 			return repo.update(movie);
-		}else{
+		} else {
 			throw new ResourceNotFoundException();
 		}
 	}
@@ -59,9 +67,21 @@ public class MovieServiceImp implements MovieService {
 	@Override
 	public void delete(String mid) throws ResourceNotFoundException {
 		Movie m = repo.findById(mid);
-		if (m != null){
+		if (m != null) {
+			List<Comment> comments = commentRepo.findByMid(mid);
+			List<Rating> ratings = ratingRepo.findByMid(mid);
+			if (comments != null) {
+				for (Comment comment : comments) {
+					commentRepo.delete(comment);
+				}
+			}
+			if (ratings != null) {
+				for (Rating rating : ratings) {
+					ratingRepo.delete(rating);
+				}
+			}
 			repo.delete(m);
-		}else{
+		} else {
 			throw new ResourceNotFoundException();
 		}
 	}
